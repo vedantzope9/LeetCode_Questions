bae4a2ad39d8c2b7b6e6b8667985544f10aaf04b
Vedant Zope <114239198+vedantzope9@users.noreply.github.com>
2023-10-03 10:06:15 +0530

    Time: 8 ms (10.36%), Space: 45.1 MB (19.26%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    void quicksort(int arr[],int low,int high)   
+    {
+        if(low>=high)
+        return;
+
+        int start=low,end=high;
+        int mid=start+(end-start)/2;
+        int pivot=arr[mid];
+
+        while(start<=end)
+        {
+            while(arr[start]<pivot)
+            start++;
+            while(arr[end]>pivot)
+            end--;
+
+            if(start<=end)
+            {
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
+        }
+        quicksort(arr,low,end);
+        quicksort(arr,start,high);
+    }
+    public int[] sortedSquares(int[] nums)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=nums[i]*nums[i];
+        }
+        
+        quicksort(nums,0,nums.length-1);
+        return nums;
+    }
+}
