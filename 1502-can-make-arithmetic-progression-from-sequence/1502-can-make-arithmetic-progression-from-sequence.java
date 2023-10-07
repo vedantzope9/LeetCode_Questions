6345d27f388defaef02856745be0cc723c75e36c
Vedant Zope <114239198+vedantzope9@users.noreply.github.com>
2023-10-07 09:42:20 +0530

    Time: 2 ms (95.60%), Space: 40.3 MB (86.02%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr)
+    {
+        Arrays.sort(arr);
+        int d=arr[1]-arr[0];
+
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i+1]-arr[i]!=d)
+            return false;
+        }
+
+        return true;
+    }
+}
