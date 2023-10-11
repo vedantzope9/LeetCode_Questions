82489a8c9a339e2a41a759bb7ec95c400e2abe7f
Vedant Zope <114239198+vedantzope9@users.noreply.github.com>
2023-10-11 07:51:58 +0530

    Time: 3 ms (75.83%), Space: 45 MB (22.40%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums)
+    {
+        int i=0;
+        while(i<nums.length)
+        {
+            int correct=nums[i]-1;
+
+            if(nums[correct]!=nums[i])
+            {
+                int temp=nums[correct];
+                nums[correct]=nums[i];
+                nums[i]=temp;
+            }
+            else 
+            i++;
+            
+        }
+
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=i+1)
+            return new int[] {nums[i],i+1};
+        }
+        return new int[] {-1,-1};
+    }
+}
