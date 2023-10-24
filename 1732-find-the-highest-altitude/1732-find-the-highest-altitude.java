d8b4614d8c57cc10d8c25c931aa4a3267fa65c13
Vedant Zope <114239198+vedantzope9@users.noreply.github.com>
2023-10-24 17:22:57 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (24.19%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,20 +1,30 @@
 class Solution {
     public int largestAltitude(int[] gain) 
     {
-        int alt[]=new int[gain.length+1];
-        alt[0]=0;
+    //     int alt[]=new int[gain.length+1];
+    //     alt[0]=0;
 
+    //     for(int i=0;i<gain.length;i++)
+    //     {
+    //         alt[i+1]=alt[i]+gain[i];
+    //     }        
+    //     int max=Integer.MIN_VALUE;
+        
+    //     for(int i=0;i<alt.length;i++)
+    //     {
+    //         if(alt[i]>max)
+    //         max=alt[i];;
+    //     }
+    // return max;
+
+        int sum=0,prev=0;
         for(int i=0;i<gain.length;i++)
         {
-            alt[i+1]=alt[i]+gain[i];
-        }        
-        int max=Integer.MIN_VALUE;
-        
-        for(int i=0;i<alt.length;i++)
-        {
-            if(alt[i]>max)
-            max=alt[i];;
+            sum=sum+gain[i];
+
+            if(sum>prev)
+            prev=sum;
         }
-    return max;
+        return prev;
     }
 }
